7cff9788c459a709f624ba6afcc5ff49607095e5
Lucas <lucas64450@hotmail.fr>
2021-07-30 22:39:19 +0200

    navbar and mobile menu responsive

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,25 +1,52 @@
 import React from 'react'
 import { FaBars } from 'react-icons/fa'
-import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NavbarElements';
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+  NavBtn,
+  NavBtnLink
+} from './NavbarElements'
 
-const Navbar = () => {
-    return (
-        <>
-        <Nav>
-            <NavbarContainer>
-                <NavLogo to="/">Le Meilleur de Soi</NavLogo>
-                <MobileIcon>
-                    <FaBars />
-                </MobileIcon>
-                <NavMenu>
-                    <NavItem>
-                        <NavLinks to="about">About</NavLinks>
-                    </NavItem>
-                </NavMenu>
-            </NavbarContainer>
-        </Nav>
-        </>
-    )
+const Navbar = ({toggle}) => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo to='/'>Le Meilleur de Soi</NavLogo>
+          <MobileIcon onClick={toggle}>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to='Accueil'>Accueil</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='seances'>Séances</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to='A propos'>A propos</NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks href='https://www.facebook.com/merveilledesprit'>
+                Facebook
+              </NavLinks>
+            </NavItem>
+            <NavItem>
+              <NavLinks to=''>Instagram</NavLinks>
+            </NavItem>
+          </NavMenu>
+          <NavBtn>
+              <NavBtnLink to='/contact'>Contact</NavBtnLink>
+          </NavBtn>
+        </NavbarContainer>
+      </Nav>
+    </>
+  )
 }
 
 export default Navbar
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
+  SideBtnWrap,
+  SidebarRoute
+} from './SidebarElements'
+
+function Sidebar ({isOpen, toggle}) {
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon onClick={toggle}>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to='Accueil' onClick={toggle}>Accueil</SidebarLink>
+          <SidebarLink to='Seances'>Séances</SidebarLink>
+          <SidebarLink to='A propos'>A propos</SidebarLink>
+          <SidebarLink to='Facebook'>Facebook</SidebarLink>
+          <SidebarLink to='Instagram'>Instagram</SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to='/contact'>Contact</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  )
+}
+
+export default Sidebar
